d82bf4c738389aa3b8caaeb79c670752b7d0adf6
swus1978 <juang4u@gmail.com>
2023-05-14 15:56:37 -0500

    new uppdate to the script

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
 const interestRate = 1.75;
 
 function calculateMonthlyQuote(loanTerm, loanAmount, interestRate) {
-    console.log ("Quote details: 👇");
+    console.log ("Quote details: right here 👇");
     breakdown = (loanAmount * interestRate) / loanTerm
     for (let i = 0; i < loanTerm; i++) {
         console.log ("Quote details" + (i + 1) + "$.MXN"+ breakdown.toFixed(2));
