1f84f4d213aa4ed65ff579304d6755d25eba367d
swus1978 <juang4u@gmail.com>
2023-05-14 15:59:06 -0500

    replaced lost file

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
 const interestRate = 1.75;
 
 function calculateMonthlyQuote(loanTerm, loanAmount, interestRate) {
-    console.log ("Quote details: right here 👇");
+    console.log ("Quote details: here 👇");
     breakdown = (loanAmount * interestRate) / loanTerm
     for (let i = 0; i < loanTerm; i++) {
         console.log ("Quote details" + (i + 1) + "$.MXN"+ breakdown.toFixed(2));
