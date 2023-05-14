bbabfab304eff0e9bebb0d0cef283e9454796898
swus1978 <juang4u@gmail.com>
2023-05-14 12:13:07 -0500

    my second change to the current

--- js/loanSimulator.js
+++ js/loanSimulator.js
@@ -1,15 +1,13 @@
 // loan simulator ask user for loan questions.
 function loanSimulator() {
     let loanAmount = parseInt(prompt("Enter loan amount"));
-    let loanTerm = parseInt(prompt("Enter length of term. Up to 36 Months"));
-    // console.log(loanAmount);
-    // console.log(loanTerm);
+    let loanTerm = parseInt(prompt("Enter length of term. Up to 52 Months"));
     if (isNaN(loanAmount) || isNaN(loanTerm)) {
         console.error("The element entered is not a number. Please try again.");
         return;
     }
 
-    if (loanTerm >= 1 && loanTerm <= 36) {
+    if (loanTerm >= 1 && loanTerm <= 52) {
         alert("In a moment you can review the breakdown on your personal" + loanTerm + "month payments. Your monthly payments total. $" + loanAmount);
         calculateMonthlyQuote(loanTerm, loanAmount, interestRate);
     } else {
