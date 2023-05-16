03ebdd316a5c1d69048dee08630cd0e185808508
swus1978 <juang4u@gmail.com>
2023-05-16 10:49:21 -0500

    changed js file name to financeCalculator.js

--- js/financeCalulator.js
+++ js/financeCalulator.js
@@ -2,7 +2,7 @@
 const interestRate = 17.00;
 
 function calculateMonthlyQuote(loanTerm, loanAmount, interestRate) {
-    console.log ("Quote details: here 👇🤑.");
+    console.log ("Quote details: here 👇🤑");
     breakdown = (loanAmount * interestRate) / loanTerm
     for (let i = 0; i < loanTerm; i++) {
         console.log ("Quote details" + (i + 1) + "$.MXN"+ breakdown.toFixed(2));
